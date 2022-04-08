a780c7b2276c21d554d23e402e4718bccee12f0b
artiumrexbellator <ait-addi@hotmail.com>
2022-04-08 02:43:05 +0000

    store imageesin sqlite 3

--- PlantChars.py
+++ PlantChars.py
@@ -4,88 +4,140 @@ import plantcv.plantcv as pcv
 import cv2
 import numpy as np
 import json
+from PIL import Image
+import io
+import base64
+
 
 class PlantChars:
+
     def __init__(self):
-        self.image=None
-        self.mask=None
-        self.skel=None
-    def loadImage(self,path):
-        self.image=cv2.imread(path)
+        self.image = None
+        self.mask = None
+        self.skel = None
+
+    def loadImage(self, path):
+        self.image = cv2.imread(path)
         # Convert RGB to HSV and extract the saturation channel
         s = pcv.rgb2gray_hsv(rgb_img=self.image, channel='s')
-        # Take a binary threshold to separate plant from background. 
+        # Take a binary threshold to separate plant from background.
         # Threshold can be on either light or dark objects in the image.
-        s_thresh = pcv.threshold.binary(gray_img=s, threshold=85, max_value=255, object_type='light')
-        # Median Blur to clean noise 
+        s_thresh = pcv.threshold.binary(gray_img=s,
+                                        threshold=85,
+                                        max_value=255,
+                                        object_type='light')
+        # Median Blur to clean noise
         s_mblur = pcv.median_blur(gray_img=s_thresh, ksize=5)
-        self.mask=s_mblur
-        self.mask=cv2.threshold(self.mask, 127, 255, cv2.THRESH_BINARY)[1]
+        self.mask = s_mblur
+        self.mask = cv2.threshold(self.mask, 127, 255, cv2.THRESH_BINARY)[1]
+
     def whiteMask(self):
         # Apply mask (for VIS images, mask_color=white)
-        mask=pcv.apply_mask(img=self.image, mask=self.mask, mask_color='white')
+        mask = pcv.apply_mask(img=self.image,
+                              mask=self.mask,
+                              mask_color='white')
         #pcv.print_image(mask, '/home/artium/Desktop/master/image processing/LeafDetector/masked.png')
         return mask
+
     def skeletonize(self):
-        skel=skeletonize(self.mask)
+        skel = skeletonize(self.mask)
         #pcv.print_image(skel, '/home/artium/Desktop/master/image processing/LeafDetector/skeleton.png')
         return skel
+
     def skeleton(self):
-        img=self.mask.copy()
+        img = self.mask.copy()
         size = np.size(img)
-        skel = np.zeros(img.shape,np.uint8)
-        
-        ret,img = cv2.threshold(img,127,255,0)
-        element = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
+        skel = np.zeros(img.shape, np.uint8)
+
+        ret, img = cv2.threshold(img, 127, 255, 0)
+        element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
         done = False
-        
-        while( not done):
-            eroded = cv2.erode(img,element)
-            temp = cv2.dilate(eroded,element)
-            temp = cv2.subtract(img,temp)
-            skel = cv2.bitwise_or(skel,temp)
+
+        while (not done):
+            eroded = cv2.erode(img, element)
+            temp = cv2.dilate(eroded, element)
+            temp = cv2.subtract(img, temp)
+            skel = cv2.bitwise_or(skel, temp)
             img = eroded.copy()
-        
+
             zeros = size - cv2.countNonZero(img)
-            if zeros==size:
+            if zeros == size:
                 done = True
         #pcv.print_image(skel, '/home/artium/Desktop/master/image processing/LeafDetector/skel.png')
         return skel
+
     def prune(self):
         #skeleton=pcv.closing(gray_img=self.skeleton())
         if self.skel is None:
-            self.skel=self.skeletonize()
-        img1, seg_img, edge_objects = prune(skel_img=self.skel, size=15,mask=self.mask)
+            self.skel = self.skeletonize()
+        img1, seg_img, edge_objects = prune(skel_img=self.skel,
+                                            size=15,
+                                            mask=self.mask)
         #pcv.print_image(img1, '/home/artium/Desktop/master/image processing/LeafDetector/pruned.png')
         return img1
+
     def findBranches(self):
-        branches=find_branch_pts(skel_img=self.prune(),mask=self.mask)
+        branches = find_branch_pts(skel_img=self.prune(), mask=self.mask)
         #pcv.print_image(branches, '/home/artium/Desktop/master/image processing/LeafDetector/branches.png')
         return branches
+
     def findTips(self):
         if self.skel is None:
-            self.skel=self.skeletonize()
-        tip_pts_mask=find_tips(skel_img=self.skel, label="tips",mask=self.mask)
+            self.skel = self.skeletonize()
+        tip_pts_mask = find_tips(skel_img=self.skel,
+                                 label="tips",
+                                 mask=self.mask)
         #pcv.print_image(tip_pts_mask, '/home/artium/Desktop/master/image processing/LeafDetector/tips.png')
         return tip_pts_mask
+
     def height(self):
         # Identify objects and contours
-        mask=self.whiteMask()
+        mask = self.whiteMask()
         id_objects, obj_hierarchy = pcv.find_objects(img=mask, mask=self.mask)
-        # Define the region of interest (ROI) 
-        roi1, roi_hierarchy= pcv.roi.rectangle(img=mask, x=100, y=100, h=200, w=200)
+        # Define the region of interest (ROI)
+        roi1, roi_hierarchy = pcv.roi.rectangle(img=mask,
+                                                x=100,
+                                                y=100,
+                                                h=200,
+                                                w=200)
         # Decide which objects to keep
-        roi_objects, hierarchy3, kept_mask, obj_area = pcv.roi_objects(img=self.image, roi_contour=roi1, 
-                                                               roi_hierarchy=roi_hierarchy, 
-                                                               object_contour=id_objects, 
-                                                               obj_hierarchy=obj_hierarchy,
-                                                               roi_type='partial')
+        roi_objects, hierarchy3, kept_mask, obj_area = pcv.roi_objects(
+            img=self.image,
+            roi_contour=roi1,
+            roi_hierarchy=roi_hierarchy,
+            object_contour=id_objects,
+            obj_hierarchy=obj_hierarchy,
+            roi_type='partial')
         # Object combine kept objects
-        obj, mask = pcv.object_composition(img=self.image, contours=roi_objects, hierarchy=hierarchy3)
-        ############### Analysis ################ 
+        obj, mask = pcv.object_composition(img=self.image,
+                                           contours=roi_objects,
+                                           hierarchy=hierarchy3)
+        ############### Analysis ################
         # Find shape properties, data gets stored to an Outputs class automatically
-        analysis_image = pcv.analyze_object(img=self.image, obj=obj, mask=self.mask, label="default")
+        analysis_image = pcv.analyze_object(img=self.image,
+                                            obj=obj,
+                                            mask=self.mask,
+                                            label="default")
         #pcv.print_image(analysis_image, '/home/artium/Desktop/master/image processing/LeafDetector/height.png')
-    
+        return analysis_image.astype("uint8")
+
+
+# u can use tostring or fromtobytes,but tobytes is optimal as long as tostring will be deprecated for behavior reasons
+
+    def encodeImage(self, img):
+        enc = cv2.imencode('.png', img)[1]
+        data_encode = np.array(enc)
+        buff_encode = data_encode.tobytes()
+        return buff_encode
+
     def fullImageTraitment(self):
-        return (self.image,self.mask,self.whiteMask(),self.prune(),self.findBranches(),self.findTips(),self.height(),self.skel,json.dumps(pcv.outputs.observations))
+        return (
+        self.encodeImage(np.uint8((self.image))),
+        self.encodeImage(np.uint8(self.mask)),
+        self.encodeImage(np.uint8(self.whiteMask())),
+        self.encodeImage(np.uint8(self.prune())),
+        self.encodeImage(np.uint8(self.findBranches())),
+        self.encodeImage(np.uint8(self.findTips())),\
+        self.encodeImage(np.uint8(self.height())),
+        self.encodeImage(np.uint8(self.skel)),
+        json.dumps(pcv.outputs.observations))
--- sqlite_init.py
+++ sqlite_init.py
@@ -1,33 +1,85 @@
 import sqlite3
 from typing import Tuple
+from PlantChars import PlantChars
+import json
+import numpy as np
+import cv2
 
 # def convertToBinaryData(filename):
 #     # Convert digital data to binary format
 #     with open(filename, 'rb') as file:
-#         blobData = file.read()
-#     return blobData
+#         textData = file.read()
+#     return textData
 
-def insertBlob(data: Tuple):
+
+# u can use frombuffer or fromstring,but frombuffer is optimal as long as fromstring will be deprecated for behavior reasons
+def decodeImage(imgBuff):
+    nparr = np.frombuffer(imgBuff, np.uint8)
+    img_decode = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return img_decode
+
+
+def createDb(name="plant"):
     try:
-      sqliteConnection = sqlite3.connect('plants.db')
-      cursor = sqliteConnection.cursor()
-      print("Connected to SQLite")
+        conn = sqlite3.connect(f'{name}.db')
+        c = conn.cursor()
+        c.execute(
+            '''CREATE TABLE plants (raw_image text, binary_image text, masked_image text, pruned_image text, branches_image text, tips_image text, height_image text, skeleton_image text,
+        data text)''')
+    except:
+        pass
 
-      query = f"""INSERT INTO 
-									plants (raw_image, binary_image, masked_image, pruned_image, banches_image, tips_image, height_image, skeleton_image, data) 
+
+def insertData(data: Tuple, name="plant"):
+    try:
+        sqliteConnection = sqlite3.connect(f'{name}.db')
+        cursor = sqliteConnection.cursor()
+        print("Connected to SQLite")
+
+        query = f"""INSERT INTO 
+									plants (raw_image, binary_image, masked_image, pruned_image, branches_image, tips_image, height_image, skeleton_image, data) 
 									VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"""
 
-      cursor.execute(query, data)
-      sqliteConnection.commit()
+        cursor.execute(query, data)
+        sqliteConnection.commit()
 
-      cursor.close()
+        cursor.close()
 
     except sqlite3.Error as error:
-        print("Failed to insert blob data into sqlite table", error)
+        print("Failed to insert text data into sqlite table", error)
     finally:
         if sqliteConnection:
             sqliteConnection.close()
             print("the sqlite connection is closed")
 
+
+def getData(name="plant"):
+    sqliteConnection = sqlite3.connect(f'{name}.db')
+    cursor = sqliteConnection.cursor()
+    query = "select * from plants"
+    images = []
+    for raw_image, binary_image, masked_image, pruned_image, branches_image, tips_image, height_image, skeleton_image, data in cursor.execute(
+            query):
+        images.append([
+            decodeImage((raw_image)),
+            decodeImage((binary_image)),
+            decodeImage((masked_image)),
+            decodeImage((pruned_image)),
+            decodeImage((branches_image)),
+            decodeImage((tips_image)),
+            decodeImage((height_image)),
+            decodeImage((skeleton_image)),
+            json.loads(data)
+        ])
+    cursor.close()
+    return images
+
+
 if __name__ == "__main__":
-	insertBlob("")
+    #createDb()
+    #p = PlantChars()
+    #p.loadImage("plants/plant.jpg")
+    #insertData(p.fullImageTraitment())
+    #p.fullImageTraitment()
+    cv2.imshow("winname", getData()[0][4])
+    k = cv2.waitKey(0)
