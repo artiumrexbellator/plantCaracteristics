65812ea99abb6c969b3e52c4c83a65bd2ee86897
Abderrahim Ajakka <aajakka@masaratapp.com>
2022-04-13 20:06:55 +0000

    Display changes

--- src/plant_sqlite.py
+++ src/plant_sqlite.py
@@ -29,23 +29,39 @@ def select_plants():
         connection = sqlite3.connect('./database/plant.db')
         cursor = connection.cursor()
         query = "SELECT * FROM plants"
+        raw_imgs = []
+        binary_imgs = []
+        masked_imgs = []
+        pruned_imgs = []
+        branches_imgs = []
+        tips_imgs = []
+        height_imgs = []
+        skeleton_imgs = []
         images = []
-        for id, raw_image, binary_image, masked_image, pruned_image, branches_image, tips_image, height_image, skeleton_image, metadata in cursor.execute(
-                query):
-            images.append({
-                "id": id,
-                "raw_image": decode_image((raw_image)),
-                "binary_image": decode_image((binary_image)),
-                "masked_image": decode_image((masked_image)),
-                "pruned_image": decode_image((pruned_image)),
-                "branches_image": decode_image((branches_image)),
-                "tips_image": decode_image((tips_image)),
-                "height_image": decode_image((height_image)),
-                "skeleton_image": decode_image((skeleton_image)),
-                "metadata": json.loads(metadata)
-            })
+        for id, raw_image, binary_image, \
+          masked_image, pruned_image, branches_image, \
+           tips_image, height_image, skeleton_image, \
+            metadata in cursor.execute(query):
+            raw_imgs.append(decode_image((raw_image)))
+            binary_imgs.append(decode_image((binary_image)))
+            masked_imgs.append(decode_image((masked_image)))
+            pruned_imgs.append(decode_image((pruned_image)))
+            branches_imgs.append(decode_image((branches_image)))
+            tips_imgs.append(decode_image((tips_image)))
+            height_imgs.append(decode_image((height_image)))
+            skeleton_imgs.append(decode_image((skeleton_image)))
+
         cursor.close()
-        return images
+        return [
+            tuple(raw_imgs),
+            tuple(binary_imgs),
+            tuple(masked_imgs),
+            tuple(pruned_imgs),
+            tuple(branches_imgs),
+            tuple(tips_imgs),
+            tuple(height_imgs),
+            tuple(skeleton_imgs),
+        ]
     except sqlite3.Error as error:
         print("Error selecting data:", error)
     finally:
